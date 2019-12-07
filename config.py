69fd60e55d66d8b9ae6d033bcd04cba812beb0f9
Yang Liu <liuyang12@focusmedia.cn>
2019-12-07 15:10:12 +0800

    update

---
+++ config.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
+
+# Model parameters
+image_w = 112
+image_h = 112
+channel = 3
+emb_size = 512
+
+# Training parameters
+num_workers = 8  # for data-loading; right now, only 1 works with h5py
+grad_clip = 5.  # clip gradients at an absolute value of
+print_freq = 100  # print training/validation stats  every __ batches
+checkpoint = None  # path to checkpoint, None if none
+
+# Data parameters
+num_classes = 85742
+num_samples = 5822653
+DATA_DIR = 'data'
+faces_ms1m_folder = 'data/faces_emore'
+path_imgidx = os.path.join(faces_ms1m_folder, 'train.idx')
+path_imgrec = os.path.join(faces_ms1m_folder, 'train.rec')
+IMG_DIR = 'data/images'
+pickle_file = 'data/faces_ms1m_112x112.pickle'
+
+
+def get_logger():
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s %(levelname)s \t%(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+logger = get_logger()
--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 from utils import extract
 
 if __name__ == "__main__":
-    # if not os.path.isdir('data_proc/cron20190326_resized'):
-    extract('data_proc/cron20190326.zip')
-    extract('data_proc/cron20190415.zip')
+    extract('data/retinaface_gt_v1.zip')
+
+
+
+
