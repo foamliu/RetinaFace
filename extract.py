78f750bf097acd47b227ac5f87413f88472df1af
Yang Liu <liuyang12@focusmedia.cn>
2019-12-07 15:13:04 +0800

    update

--- extract.py
+++ extract.py
@@ -1,8 +1,12 @@
-from utils import extract
+import zipfile
+
+
+def extract(filename):
+    print('Extracting {}...'.format(filename))
+    zip_ref = zipfile.ZipFile(filename, 'r')
+    zip_ref.extractall('data')
+    zip_ref.close()
+
 
 if __name__ == "__main__":
     extract('data/retinaface_gt_v1.zip')
-
-
-
-
