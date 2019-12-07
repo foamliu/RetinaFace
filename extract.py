812263c6c67d99b5c8721050d09ea58061f1b1ab
Yang Liu <liuyang12@focusmedia.cn>
2019-12-07 15:25:37 +0800

    update

--- extract.py
+++ extract.py
@@ -1,15 +1,17 @@
 import zipfile
+from utils import ensure_folder
 
-
-def extract(filename):
+def extract(filename, tgt_folder='data'):
     print('Extracting {}...'.format(filename))
     zip_ref = zipfile.ZipFile(filename, 'r')
-    zip_ref.extractall('data')
+    zip_ref.extractall(tgt_folder)
     zip_ref.close()
 
 
 if __name__ == "__main__":
-    extract('data/retinaface_gt_v1.1.zip')
+    tgt_folder = 'data/widerface'
+    ensure_folder(tgt_folder)
+    extract('data/retinaface_gt_v1.1.zip', tgt_folder)
     extract('data/WIDER_train.zip')
     extract('data/WIDER_val.zip')
     extract('data/WIDER_test.zip')
